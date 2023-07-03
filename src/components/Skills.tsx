47a5c5a0f4794f2faa9596c2adad0d2b7a57c1e7
HarCass <harry.jc.cass@gmail.com>
2023-07-03 13:34:17 +0100

    Added testing skills tab and changed icons to SVG files removing react-icons dependancy

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import {SiTypescript, SiExpress, SiMongodb, SiPostgresql, SiReact, SiJavascript, SiNodedotjs, SiHtml5, SiCss3} from 'react-icons/si';
 
 const Skills = () => {
     const [tab, setTab] =  useState("lang");
@@ -8,65 +7,87 @@ const Skills = () => {
         setTab(tabName);
     }
 
-    return <div className='p-1 pt-0 bg-slate-900 rounded text-center animate-fade-up animate-duration-1000 animate-delay-1000 h-[350px]'>
+    return <div className='pt-0 bg-slate-900 rounded text-center animate-fade-up animate-duration-1000 animate-delay-1000 h-[350px]'>
         <div>
             <button className='p-1 bg-slate-900 rounded-s-md md:text-2xl shadow-inner shadow-slate-400 hover:bg-slate-800' onClick={() => tabHandler("lang")}>Languages</button>
             <button className='p-1 bg-slate-900 md:text-2xl shadow-inner shadow-slate-400 hover:bg-slate-800' onClick={() => tabHandler("front")}>Frontend</button>
             <button className='p-1 bg-slate-900 rounded-e-md md:text-2xl shadow-inner shadow-slate-400 hover:bg-slate-800' onClick={() => tabHandler("back")}>Backend</button>
+            <button className='p-1 bg-slate-900 rounded-e-md md:text-2xl shadow-inner shadow-slate-400 hover:bg-slate-800' onClick={() => tabHandler("test")}>Testing</button>
         </div>
-        {tab === "lang" ? <Languages/> : tab === "front" ? <Frontend/> : <Backend/>}
+        {tab === "lang" ? <Languages/> : tab === "front" ? <Frontend/> : tab === "back" ? <Backend/> : <Testing/>}
     </div>
 }
 
 const Languages = () => {
     return <ul className="animate-fade-up animate-duration-1000">
-        <li>
-            <SiJavascript className="rounded-sm text-4xl inline" style={{color: "#ffd500"}}/>
-            <p className='text-xl md:text-2xl inline'>JavaScript</p>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/javascript.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>JavaScript</p>
         </li>
-        <li>
-            <SiTypescript className="text-4xl inline" style={{color: "deepskyblue"}}/>
-            <p className='text-xl md:text-2xl inline'>TypeScript</p>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/typescript.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>TypeScript</p>
         </li>
     </ul>
 }
 
 const Frontend = () => {
     return <ul className="animate-fade-up animate-duration-1000">
-        <li>
-            <SiHtml5 className='text-4xl' style={{color: "#ed461d"}}/>
-            <p className='text-xl md:text-2xl inline'>HTML</p>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/html.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>HTML</p>
         </li>
-        <li>
-            <SiCss3 className='text-4xl' style={{color: "#2bb7fd"}}/>
-            <p className='text-xl md:text-2xl inline'>CSS</p>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/css.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>CSS</p>
         </li>
-        <li>
-            <SiReact className='text-4xl' style={{color: "#2edcff"}}/>
-            <p className='text-xl md:text-2xl inline'>React</p>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/react.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>React</p>
+        </li>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/react-native.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>React Native</p>
         </li>
     </ul>
 }
 
 const Backend = () => {
     return <ul className="animate-fade-up animate-duration-1000">
-        <li>
-            <SiNodedotjs className='text-4xl' style={{color: "#2f8f2d"}}/>
-            <p className='text-xl md:text-2xl inline'>NodeJS</p>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/nodejs.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>NodeJS</p>
         </li>
-        <li>
-            <SiExpress className='text-4xl'/>
-            <p className='text-xl md:text-2xl inline'>ExpressJS</p>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/expressjs.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>ExpressJS</p>
         </li>
-        <li>
-            <SiMongodb className='text-4xl' style={{color: "lawngreen"}}/>
-            <p className='text-xl md:text-2xl inline'>MongoDB</p>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/mongodb.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>MongoDB</p>
         </li>
-        <li>
-            <SiPostgresql className="text-4xl rounded-sm p-1" style={{backgroundColor: "cornflowerblue"}}/>
-            <p className='text-xl md:text-2xl inline'>PostgresQL</p>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/postgresql.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>PostgresQL</p>
         </li>
     </ul>
+}
+
+const Testing = () => {
+    return <ul className="animate-fade-up animate-duration-1000">
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/jest.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>Jest</p>
+        </li>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/mocha.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>Mocha</p>
+        </li>
+        <li className="mt-2">
+            <img src="https://harcass.github.io/profile-site/logos/cypress.svg" className="h-10 inline"></img>
+            <p className='ml-2 text-xl md:text-2xl inline'>Cypress</p>
+        </li>
+    </ul> 
 }
 
 export default Skills;
